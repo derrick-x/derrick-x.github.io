7a27b85d40922272d2dddb827a7af62fcff2d3cf
Derrick <derrickx101@gmail.com>
2025-06-30 13:11:07 -0700

    canvas test

--- assets/js/mlm-script.js
+++ assets/js/mlm-script.js
@@ -1,7 +1,16 @@
 window.onload = function() {
     console.log("success!");
-    const canvas = document.getElementById("myCanvas");
-    const ctx = canvas.getContext("2d");
-    ctx.fillStyle = "rgb(0, 0, 0)";
-    ctx.fillRect(0, 0, 500, 300);
+    const ctx = [document.getElementById("feed-forward").getContext("2d")];
+}
+
+function drawFeedForward(id) {
+    ctx[id].fillStyle = "rgb(0, 0, 0)";
+    ctx[id].fillRect(0, 0, 500, 300);
+}
+
+function drawBackpropagation(id) {
+}
+
+function drawTokenization(id) {
+
 }
